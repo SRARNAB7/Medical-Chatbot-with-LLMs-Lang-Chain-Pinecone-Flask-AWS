d92197175063b6a07aa00e4b3d5fe6fb26b1eb06
SRARNAB7 <asutar1@slu.edu>
2026-04-09 17:34:55 -0500

    medicalbot

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Arnab Sutar",
+    author_email="srarnab7@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
